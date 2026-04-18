e5f0330a8d436864bbae60c94d172e5e7bc47aae
joakoszxx <joakotier1acc@gmail.com>
2026-04-17 19:23:51 -0500

    caca

---
+++ commands/sign.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('test')
+    .setDescription('test'),
+
+  async execute(interaction) {
+    await interaction.reply('ok');
+  }
+};
