078668d14d2d208bdfc940715cf3deabc81eab0b
joakoszxx <ortizjoaquin380@gmail.com>
2026-04-17 20:11:42 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,9 +1,11 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
-const {GUILD_ID} = "1494859005061894369";
+
+const CLIENT_ID = "1494859005061894369"; // tu bot
+const GUILD_ID = "1483641603955490966"; // 👈 CAMBIA ESTO
 
 const commands = [];
-const files = fs.readdirSync('./commands');
+const files = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
 
 for (const file of files) {
   const command = require(`./commands/${file}`);
@@ -13,8 +15,16 @@ for (const file of files) {
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-  await rest.put(
-    Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-    { body: commands }
-  );
+  try {
+    console.log("🚀 Registrando comandos...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    );
+
+    console.log("✅ Comandos registrados!");
+  } catch (error) {
+    console.error(error);
+  }
 })();
