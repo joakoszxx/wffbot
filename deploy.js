5b60039baf63617fca57805a514fe5029d2bf1fd
joakoszxx <ortizjoaquin380@gmail.com>
2026-04-17 20:04:50 -0500

    Add deploy script for Discord bot commands

---
+++ deploy.js
@@ -0,0 +1,19 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const commands = [];
+const files = fs.readdirSync('./commands');
+
+for (const file of files) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
+})();
