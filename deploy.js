a2298341a8ae158b6deac5a35361fb5922c177ad
joakoszxx <joakotier1acc@gmail.com>
2026-04-17 20:16:56 -0500

    WFF Bot Made By @joakoszn. version 1.0.0

---
+++ deploy.js
@@ -0,0 +1,30 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const CLIENT_ID = "1494859005061894369"; // tu bot
+const GUILD_ID = "1483641603955490966"; // 👈 CAMBIA ESTO
+
+const commands = [];
+const files = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+
+for (const file of files) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("🚀 Registrando comandos...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    );
+
+    console.log("✅ Comandos registrados!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
